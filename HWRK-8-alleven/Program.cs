e5aa9a0c8beb9844f7f52efa4e3b39c5c9504fd4
Cassandra <cassandra01@yandex.ru>
2022-09-20 17:26:16 +0300

    добавлена задача про вывод всех чётных чисел до введённого числа

---
+++ HWRK-8-alleven/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int inputnumbers()
+{
+    int number = 0;
+    bool isConverted = false;
+    while (isConverted != true)
+    {
+        Console.Write($"Введите число (целое): ");
+        string input1 = Console.ReadLine();
+        try
+        {
+            number = Convert.ToInt32(input1);
+            isConverted = true;
+        }
+        catch (FormatException)
+        {
+            isConverted = false;
+            Console.WriteLine("Неправильно задано число");
+        }
+    }
+    return number;
+}
+
+Console.WriteLine("Программа принимает число и выводит все чётные числа от 1 до введённого числа.");
+Console.WriteLine("Для работы программы необходимо задать число.");
+
+int numberA=inputnumbers();
+
+int count=1;
+
+Console.Write($"Чётные числа от 1 до {numberA}: ");
+while (count<=numberA)
+{
+    if (count%2==0)
+    {
+        Console.Write(count+" ");
+    }
+    count++;
+}
