41c42f9d54d67e61502a9db9256e9fb29fc2f1e0
Cassandra <cassandra01@yandex.ru>
2022-09-20 09:45:04 +0300

    добавлена 7 задача про треугольник

---
+++ Exl006-hiuniqueuser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа приветствия конкретного пользователя");
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "вася")
+{
+    Console.WriteLine("Привет, Васян! Давно не виделись. Как оно?");
+}
+else
+{
+    Console.WriteLine("Здравствуйте, пользователь " + username);
+}
---
+++ Exl007-triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+int xa = 40, ya = 1,
+xb = 1, yb = 30,
+xc = 80, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+int count = 0;
+
+while (count < 5000)
+{
+    int point = new Random().Next(0, 3);
+    if (point == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    else if(point == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    else
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
