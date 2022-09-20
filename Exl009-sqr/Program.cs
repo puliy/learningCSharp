881f554aa66610b0765185dd2264fc960d3963cc
Cassandra <cassandra01@yandex.ru>
2022-09-20 10:31:51 +0300

    добавлена задача про дни недели. решена через массив, а не через 7 условий, значит, видимо, неправильно хехехе

--- Exl009-sqr/Program.cs
+++ Exl009-sqr/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Написать программу, которая на вход принимает 2 числа и проверяет, является ли первое квадратом второго
 Console.Write("Введите первое число: ");
-int number1= int.Parse(Console.ReadLine());
+int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int number2= int.Parse(Console.ReadLine());
-if (number2==Math.Pow(number1,2))
+int number2 = int.Parse(Console.ReadLine());
+if (number2 == Math.Pow(number1, 2))
 {
     Console.WriteLine($"Да, первое число {number1} является квадратом второго {number2}");
 }
---
+++ Exl010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая будет выдавать название дня недели по заданному номеру
+Console.Write("Введите номер дня недели: ");
+int weekdaynum = int.Parse(Console.ReadLine());
+string[] dayofweek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+if (weekdaynum < 1 || weekdaynum > 7)
+{
+    Console.WriteLine("Неправильно задан номер дня недели. Остаётесь без ответа.");
+}
+else
+{
+    Console.WriteLine("Ваш день недели - " + dayofweek[weekdaynum-1]);
+}
