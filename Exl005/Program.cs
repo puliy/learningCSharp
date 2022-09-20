5a07671f625430f3a7191cec944ffd708d3516c2
Cassandra <cassandra01@yandex.ru>
2022-09-20 09:10:45 +0300

    пятая задача добавлена

---
+++ Exl005/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Программа умножения двух случайных чисел");
+double numberA = new Random().Next(1, 30), numberB = new Random().Next(1, 20);
+Console.WriteLine("Число 1: " + numberA);
+Console.WriteLine("Число 2: " + numberB);
+double result = numberA * numberB;
+Console.WriteLine("Результат умножения: " + result);
